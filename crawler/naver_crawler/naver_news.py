c3521112cf488f0cda32db06d42644753e3f72a4
bhkim <bhkim0620@naver.com>
2021-09-16 01:47:19 +0900

    add date and headless option

--- crawler/naver_crawler/naver_news.py
+++ crawler/naver_crawler/naver_news.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from multiprocessing import Pool
-import pandas as pd
 
 
 def find_last_page(url):
@@ -15,20 +14,33 @@ def find_last_page(url):
     return result.text
 
 
-def crawling_news(url):
-    res = {'title': []}
-    driver = webdriver.Chrome('./chromedriver.exe')
+def crawling_news(path):
+    date = '20210915'
+    menu = {
+        '100': '정치', '101': '경제', '102': '사회', '103': '생활/문화', '104': '세계', '105': 'IT/과학'
+    }
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+
+    url = f'https://news.naver.com/main/list.naver?mode=LSD&mid=sec&date={date}&sid1=' + path
     pages = int(find_last_page(url))
     for i in range(pages):
         print(f'{i+1}/{pages}')
         driver.get(url + f'&page={i+1}')
         results = driver.find_elements_by_xpath('//*[@id="main_content"]/div/ul/li/dl/dt/a[@class="nclicks(fls.list)"]')
         for result in results:
-            if result.text == '동영상기사': continue
-            res['title'].append(result.text)
-    return res
+            if (result.text == '동영상기사') or (result.text == ''):
+                continue
+            # print(f'{menu[path]}: {result.text}')
+    driver.close()
+
 
 if __name__ == '__main__':
-    urls = ['https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1='+str(i) for i in range(100, 106)]
-    pool = Pool(processes=4)
+    urls = [str(i) for i in range(100, 106)]
+    pool = Pool(processes=8)
     pool.map(crawling_news, urls)
+
